266cde527da63deca1d6f5303c7d15fc242be631
Lytho <thisisexis@googlemail.com>
2017-09-06 18:51:52 +0200

    - Remove some PrintMessage which I forgot :D

--- lua/mapvote/sv_confighelper.lua
+++ lua/mapvote/sv_confighelper.lua
@@ -8,12 +8,10 @@ end
 
 function ConfigHelper:ReadConfig(configFile) 
     local jsonString = file.Read("lythos_mapvote/" .. configFile .. ".txt", "DATA")
-    PrintMessage(HUD_PRINTTALK, "READ lythos_mapvote/" .. configFile .. ".txt")
     return util.JSONToTable(jsonString)
 end
 
 function ConfigHelper:WriteConfig(configFile, config) 
     local configString = util.TableToJSON(config, true) -- true = prettyPrint
     file.Write("lythos_mapvote/" .. configFile .. ".txt", configString)
-    PrintMessage(HUD_PRINTTALK, "WRITE lythos_mapvote/" .. configFile .. ".txt")
 end
